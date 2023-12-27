0095f641bb84fc9d670813d3e6cdb8440af8bedc
Thor Dossche <dossche.thor@gmail.com>
2023-12-27 13:51:12 +0100

    migrite test cli option to the test_utils

--- src/cli.py
+++ src/cli.py
@@ -1,17 +1,9 @@
 import click
 import os
 import requests
-import pickle
-from database_layer import DatabaseLayer, ModelType
+from model_types import ModelType
 from zip_utils import zip_directory
-from test_utils import generate_data, train_and_save_gluon, train_and_save_sklearn
 from pprint import pprint
-from tempfile import TemporaryDirectory, TemporaryFile
-
-
-########################################################################################################################
-# CLI
-########################################################################################################################
 
 BASE_URL = 'http://localhost:5000'
 
@@ -62,70 +54,6 @@ def upload(model_name, file_path, model_type):
 def delete(model_name):
     response = requests.delete(f'{BASE_URL}/{model_name}')
     click.echo(response.text)
-
-
-########################################################################################################################
-# Testing
-########################################################################################################################
-
-@cli.command()
-@click.argument('model_name')
-def insert_gluon(model_name):
-    with TemporaryDirectory() as tmpdir:
-        train_df, _ = generate_data()
-        train_and_save_gluon(train_df, tmpdir)
-        model = zip_directory(tmpdir)
-        DatabaseLayer().add_model(model_name, ModelType.TABULAR_PREDICTOR, model)
-
-
-@cli.command()
-@click.argument('model_name')
-def insert_sklearn(model_name):
-    with TemporaryFile() as tmpfile:
-        train_df, _ = generate_data()
-        train_and_save_sklearn(train_df, tmpfile)
-        tmpfile.seek(0)
-        DatabaseLayer().add_model(model_name, ModelType.MLP_REGRESSOR, tmpfile.read())
-
-
-@cli.command()
-@click.argument('model_name')
-def test_api(model_name):
-    _, test_data = generate_data()
-    test_data = test_data.drop(columns=['target']).to_dict('records')
-
-    data = {'features': test_data}
-    pprint(data)
-
-    response = requests.post(f'{BASE_URL}/{model_name}/predict', json=data)
-    if response.status_code == 200:
-        pprint(response.json())
-    else:
-        click.echo(response.content)
-
-
-@cli.command()
-@click.argument('model_name')
-def test_revenue(model_name):
-    with open('revenue.pkl', 'br') as f:
-        test_data = pickle.load(f)
-
-    test_data = test_data.to_dict('records')[0]
-    test_data["Stores 1km"] = 0
-    del test_data["start_date"]
-    del test_data["end_date"]
-    del test_data["period"]
-
-    test_data = [test_data]
-
-    data = {'features': test_data}
-    pprint(data)
-
-    response = requests.post(f'{BASE_URL}/{model_name}/predict', json=data)
-    if response.status_code == 200:
-        pprint(response.json())
-    else:
-        click.echo(response.content)
 
 
 if __name__ == '__main__':
--- src/test_utils.py
+++ src/test_utils.py
@@ -1,10 +1,18 @@
+import click
+from pprint import pprint
+from model_types import ModelType
+from database_layer import DatabaseLayer
 from autogluon.tabular import TabularPredictor
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from tempfile import TemporaryDirectory, TemporaryFile
+from zip_utils import *
 from random import random
 import pandas as pd
-
 import pickle
+import requests
+
+BASE_URL = 'http://localhost:5000'
 
 def generate_data():
     X = [random()*10 - 5 for _ in range(400)]
@@ -24,3 +32,72 @@ def train_and_save_sklearn(train_data, temp_file):
     pickle.dump(regressor, temp_file)
 
 
+########################################################################################################################
+# Testing CLI
+########################################################################################################################
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('model_name')
+def insert_gluon(model_name):
+    with TemporaryDirectory() as tmpdir:
+        train_df, _ = generate_data()
+        train_and_save_gluon(train_df, tmpdir)
+        model = zip_directory(tmpdir)
+        DatabaseLayer().add_model(model_name, ModelType.TABULAR_PREDICTOR, model)
+
+
+@cli.command()
+@click.argument('model_name')
+def insert_sklearn(model_name):
+    with TemporaryFile() as tmpfile:
+        train_df, _ = generate_data()
+        train_and_save_sklearn(train_df, tmpfile)
+        tmpfile.seek(0)
+        DatabaseLayer().add_model(model_name, ModelType.MLP_REGRESSOR, tmpfile.read())
+
+
+@cli.command()
+@click.argument('model_name')
+def test_api(model_name):
+    _, test_data = generate_data()
+    test_data = test_data.drop(columns=['target']).to_dict('records')
+
+    data = {'features': test_data}
+    pprint(data)
+
+    response = requests.post(f'{BASE_URL}/{model_name}/predict', json=data)
+    if response.status_code == 200:
+        pprint(response.json())
+    else:
+        click.echo(response.content)
+
+
+@cli.command()
+@click.argument('model_name')
+def test_revenue(model_name):
+    with open('revenue.pkl', 'br') as f:
+        test_data = pickle.load(f)
+
+    test_data = test_data.to_dict('records')[0]
+    test_data["Stores 1km"] = 0
+    del test_data["start_date"]
+    del test_data["end_date"]
+    del test_data["period"]
+
+    test_data = [test_data]
+
+    data = {'features': test_data}
+    pprint(data)
+
+    response = requests.post(f'{BASE_URL}/{model_name}/predict', json=data)
+    if response.status_code == 200:
+        pprint(response.json())
+    else:
+        click.echo(response.content)
+
+if __name__ == '__main__':
+    cli()
